8fd763ea1e5ac251c060cfb3f89a3010aa36bd9e
Owilly <pasaclouma54@gmail.com>
2022-05-09 06:22:41 +0300

    add google calendar with draft

--- calendar/routes/api.route.js
+++ calendar/routes/api.route.js
@@ -1,4 +1,14 @@
  const router = require('express').Router();
+ const { google } = require('googleapis')
+
+  const GOOGLE_CLIENT_ID = '708105023641-t350k3usoo751tfnilu9hrdokjmmli8v.apps.googleusercontent.com'
+  const GOOGLE_CLIENT_SECRET = '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'
+
+ const oauth2Client = new google.auth.OAuth2(
+   GOOGLE_CLIENT_ID,
+   GOOGLE_CLIENT_SECRET,
+   'http://localhost:3000'
+ )
 
 router.get('/', async (req, res, next) => {
   res.send({ message: 'Ok api is working 🚀' });
@@ -6,13 +16,53 @@ router.get('/', async (req, res, next) => {
 
 router.post('/create-tokens', async (req, res, next) => {
   try {
-
+    // NEXT LINE TOKEN FROM CONSOLE
+    const REFRESH_TOKEN =
+    ''
     const { code } = req.body
-    res.send(code)
+    const { tokens } = await oauth2Client.getToken(code)
+    res.send(tokens) // DO NOT SEND BACK THE TOKEN IN THE APP
 
   } catch (error) {
     next(error)
   }
+})
+
+// cREATING EVENT TOT THE GOOGLE CALENDER FOR AUTHENTICATED USERS
+
+router.post('/create-event', async (req, res, next) => {
+try {
+  const {
+    summary, 
+    description, 
+    location, 
+    startDateTime, 
+    endDatetime,
+
+  } = req.body
+
+  oauth2Client.setCredentials({refresh_tken: REFRESH_TOKEN})
+  const calendar = google.calendar('v3')
+  const response = await calendar.events.insert({
+    auth: oauth2Client,
+    calendarId: 'primary',
+    requestBody: {
+      summary,
+      description: description,
+      location: location,
+      colorId: '6',
+      start: {
+        dateTime: new Date(startDateTime)
+      },
+      end: {
+        dateTime: new Date(endDatetime)
+      },
+    },
+  })
+  res.send(response)
+} catch (error) {
+  next(error)
+}
 })
 
 module.exports = router;
--- src/components/pages/Session.js
+++ src/components/pages/Session.js
@@ -2,43 +2,112 @@ import React, { Component } from 'react';
 import './Session.css'
 import { GoogleLogin } from 'react-google-login'
 import axios from 'axios';
+import { useState } from 'react'
+
 
 function Session() {
 
   const responseGoogle = response => {
     console.log(response)
     const { code } = response
-    axios.post('http://localhost:4000/api/create-tokens', { code })
+    axios.post('/api/create-tokens', { code })
       .then(response => {
         console.log(response.data)
+        setSignedIn(true)
       })
       .catch(error => console.log(error.message))
   }
 
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    // console.log(summary, description, location, startDateTime, endDatetime)
+    axios.post('/api/create-event', {
+      summary,
+      description,
+      location,
+      startDateTime,
+      endDatetime,
+    })
+    .then(response => {
+      console.log(response.data)
+      setSignedIn(true)
+    })
+    .catch(error => console.log(error.message))
+
+  }
+  
+
   const responseError = error => {
     console.log(error)
   }
+
+  const [summary, setSummary] = useState('')
+  const [description, setDescription] = useState('')
+  const [location, setLocation] = useState('')
+  const [startDateTime, setStartDateTime] = useState('')
+  const [endDatetime, setEndDateTime] = useState('')
+  const [signedIn, setSignedIn] = useState(false)
 
   return (
       <div>
         <div className="App">
           <h1>Add Sessions and Events</h1>
           </div>
-            <div>
-              <GoogleLogin 
-                  clientId="708105023641-t350k3usoo751tfnilu9hrdokjmmli8v.apps.googleusercontent.com"
-                  buttonText="Add sessions and events"
-                  onSuccess={responseGoogle}
-                  onFailure={responseError}
-                  cookiePolicy={'single_host_origin'}
 
-                  responseType="code"
-                  accessType="offline"
-                  scope="openid email profile https://www.googleapis.com/auth/calendar"
-                  />
-      </div>
-  </div>
-  )
+              {
+                !signedIn ? (
+                  <div>
+                  <GoogleLogin 
+                      clientId="708105023641-t350k3usoo751tfnilu9hrdokjmmli8v.apps.googleusercontent.com"
+                      buttonText="Add sessions and events"
+                      onSuccess={responseGoogle}
+                      onFailure={responseError}
+                      cookiePolicy={'single_host_origin'}
+    
+                      responseType="code"
+                      accessType="offline"
+                      scope="openid email profile https://www.googleapis.com/auth/calendar"
+                      />
+          </div>
+                ) : (
+
+      <div>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor='summary'>summary</label>
+        <br />
+        <input type='text' id='summary' value={summary} onChange={e => setSummary(e.target.value) } />
+      <br />
+
+      <label htmlFor='description'>Description</label>
+        <br />
+        <textarea id='description' value={description} onChange={e => setDescription (e.target.value)} />
+      <br />
+
+      <label htmlFor='location'>location</label>
+        <br />
+        <input type='text' id='locaion' value={location} onChange={e => setLocation (e.target.value)} />
+      <br />
+
+      <label htmlFor='startDateTime'>Starting at</label>
+        <br />
+        <input type='text' id='startdate' value={startDateTime} onChange={e => setStartDateTime} />
+      <br />
+
+      <label htmlFor='endDateTime'>End date</label>
+        <br />
+        <input type='datetime-local' id='enddate' value={endDatetime} onChange={e => setEndDateTime} />
+      <br />
+      <button type='submit'>Create event</button>
+      </form>
+    </div>
+
+  
+)
+}
+</div>
+)
+
 }
 
 export default Session
