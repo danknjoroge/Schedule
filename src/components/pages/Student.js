fea8199a98ba841ccdfe740eeac6876928600e35
Owilly <pasaclouma54@gmail.com>
2022-05-10 15:50:07 +0300

    add sidebar

--- src/components/pages/Student.js
+++ src/components/pages/Student.js
@@ -2,98 +2,44 @@ import React from "react";
 import { Container, Row, Col } from 'react-grid-system';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card'  
+import './Student.css';
 
 const Student = () => {
     return (
-<Container fluid  style={{ height: "100%" }}>
-  <Row style={{ height: "100%" }}>
-    <Col sm={2} style={{background: "#18183D", color:"white", overflow: 'hidden'}} >
-    <h3>All</h3> 
-    <Card.Link style={{ color: "white", textDecoration: "none"}}href="/student">Courses</Card.Link>   
-    <h6>Students</h6>
-    <Card.Link style={{color: "white", textDecoration: "none"}}href="/sessions">Sessions</Card.Link>   
 
-    </Col>
-    <Col sm={10}>
-        <h3 style={{textAlign:'center'}}>Courses</h3>
-        <h4  style={{background: "#18183D", borderRadius: '15px',color:"white",textAlign:'center', overflow: 'hidden',}} >Become a professional in just 25 weeks!</h4>
-   <Row>
-   <Col style={{gap: "2em"}}>
-   <Card style={{ width: '17rem',borderRadius: '15px', backgroundColor: "#fff", outline: "none", border: "none"}}>
-  <Card.Body className="test">
-    <Card.Title>Finance  & Accounting</Card.Title>
-    <Card.Text style={{color: "#18183d"}}>
-    90% employment rate according to the market.
-    </Card.Text>   
+<div class="s-layout">
+<div class="s-layout__sidebar">
+  <a class="s-sidebar__trigger" href="#0">
+     <i class="fa fa-bars"></i>
+  </a>
 
-  <style type="text/css">
-    {`
-    .btn-flat {
-      background-color: #18183D;
-      color: white;
-    }
+  <nav class="s-sidebar__nav">
+     <ul>
+        <li>
+           <a class="s-sidebar__nav-link" href="#0">
+              <i class="fa fa-home"></i><em>Home</em>
+           </a>
+        </li>
+        <li>
+           <a class="s-sidebar__nav-link" href="#0">
+             <i class="fa fa-user"></i><em>My Profile</em>
+           </a>
+        </li>
+        <li>
+           <a class="s-sidebar__nav-link" href="#0">
+              <i class="fa fa-camera"></i><em>Camera</em>
+           </a>
+        </li>
+     </ul>
+  </nav>
+</div>
 
-    .btn-sm {
-      padding: 0.1rem 0.5rem;
-      font-size: 0.9rem;
-    }
-
-    .test{
-
-    }
-    `}
-  </style>
-
-  <Button variant="flat" size="sm">
-    Take Course
-  </Button>
-
-</Card.Body>
-    </Card>
-
-    </Col>   
-    <Col>
-   <Card style={{ width: '13rem',borderRadius: '15px', backgroundColor: "#fff", outline: "none", border: "none"}}>
-  <Card.Body className="test">
-    <Card.Title>Software Engineering</Card.Title>
-    <Card.Text style={{color: "#18183d"}}>
-    92.1% employment rate according to the market.
-    </Card.Text>   
-    
- 
-
-  <Button variant="flat" size="sm">
-    Take Course
-  </Button>
-
-</Card.Body>
-    </Card>
-
-    </Col> 
-    <Col>
-   <Card style={{ width: '13rem',borderRadius: '15px', backgroundColor: "#fff", outline: "none", border: "none"}}>
-  <Card.Body className="test">
-    <Card.Title>Data <br></br> Science</Card.Title>
-    <Card.Text style={{color: "#18183d"}}>
-    89% employment rate according to the market.
-    </Card.Text>   
-    
-
-  <Button variant="flat" size="sm">
-    Take Course
-  </Button>
-
-</Card.Body>
-    </Card>
-
-    </Col> 
-
-   </Row>
-    </Col>
-  </Row>
-</Container>
-
-);
-};
+<main class="s-layout__content">
+  <h1>Full View, Please!</h1>
+</main>
+</div>
+    )
+}
 
 export default Student;
+
