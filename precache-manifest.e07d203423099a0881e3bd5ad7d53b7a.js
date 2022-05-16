dcda408dc23b2804363fb267feac0d2d1ae9de95
danknjoroge <dank8505@gmail.com>
2022-05-16 09:20:35 +0300

    Updates

---
+++ precache-manifest.e07d203423099a0881e3bd5ad7d53b7a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9628286001c968095d439c25693a3372",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5f7c54b9e6a67a9e123e",
+    "url": "/static/css/2.68a81a89.chunk.css"
+  },
+  {
+    "revision": "c2618dd082bf41524589",
+    "url": "/static/css/main.741a164a.chunk.css"
+  },
+  {
+    "revision": "5f7c54b9e6a67a9e123e",
+    "url": "/static/js/2.b4b74ffe.chunk.js"
+  },
+  {
+    "revision": "dae86f5fc8b3310e280e2f23dfa220ee",
+    "url": "/static/js/2.b4b74ffe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2618dd082bf41524589",
+    "url": "/static/js/main.c390f665.chunk.js"
+  },
+  {
+    "revision": "6fc4f2c3fd5ae8d05fd1",
+    "url": "/static/js/runtime-main.4ba1596e.js"
+  }
+]);
